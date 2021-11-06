11a72d8cf700568aca952f276743164fe0ded521
MrTwister <artemburnovich@outlook.com>
2021-11-06 22:12:42 +0300

    APDATE deep apdate of main class

--- src/com/company/House.java
+++ src/com/company/House.java
@@ -5,11 +5,11 @@ public class House {
     private int count_of_floors;
     private int count_of_flats;
     private int number_of_house;
-    public ArrayList<Floor> floors = new ArrayList<>();
+    private  ArrayList<Floor> floors = new ArrayList<>();
 
     public class Floor {
         private int number_of_floor;
-        public ArrayList<Flat> flats_on_floor = new ArrayList<>();
+        private ArrayList<Flat> flats_on_floor = new ArrayList<>();
 
         public class Flat {
             private int count_of_humans;
@@ -25,6 +25,7 @@ public class House {
                 price = 0;
                 number_of_flat = new_number_of_flat;
             }
+
             Flat(int new_count_of_humans, int new_count_of_rooms, double new_square, int new_price, int new_number_of_flat) {
                 count_of_humans = new_count_of_humans;
                 count_of_rooms = new_count_of_rooms;
@@ -36,30 +37,39 @@ public class House {
             public int Get_count_of_humans() {
                 return count_of_humans;
             }
+
             public int Get_count_of_rooms() {
                 return count_of_rooms;
             }
+
             public double Get_square() {
                 return square;
             }
+
             public int Get_price() {
                 return price;
             }
+
             public int Get_number_of_flat() {
                 return number_of_flat;
             }
+
             public void Set_count_of_humans(int new_count_of_humans) {
                 count_of_humans = new_count_of_humans;
             }
+
             public void Set_count_of_rooms(int new_count_of_rooms) {
                 count_of_rooms = new_count_of_rooms;
             }
+
             public void Set_square(double new_square) {
                 square = new_square;
             }
+
             public void Set_price(int new_price) {
                 price = new_price;
             }
+
             public void Print_Flat() {
                 System.out.println("          Number of flat: " + number_of_flat +
                         "\n          Count of humans: " + count_of_humans +
@@ -77,77 +87,117 @@ public class House {
             count_of_flats++;
             flats_on_floor.add(new Flat(new_count_of_humans, new_count_of_rooms, new_square, new_price, count_of_flats));
         }
+
         public void Add_flat() {
             count_of_flats++;
             flats_on_floor.add(new Flat(count_of_flats));
 
         }
+
         public int Get_count_of_flats_on_floor() {
             return flats_on_floor.size();
         }
+
         public int Get_number_of_floor() {
             return number_of_floor;
         }
+
         public int[] Get_numbers_of_flats_on_floor() {
             int size = flats_on_floor.size();
-            int Array[] = new int[size];
+            int[] Array = new int[size];
             for (int i = 0; i < size; i++) {
                 Array[i] = flats_on_floor.get(i).Get_number_of_flat();
             }
             return Array;
         }
-        public int Get_Count_Of_Humans_On_Floor(){
+
+        public int Get_Count_Of_Humans_On_Floor() {
             int sum = 0;
-            for (int i = 0; i < Get_count_of_flats_on_floor(); i++){
+            for (int i = 0; i < Get_count_of_flats_on_floor(); i++) {
                 sum += flats_on_floor.get(i).count_of_humans;
             }
             return sum;
         }
-        public int Get_Square_Of_Floor(){
+
+        public int Get_Square_Of_Floor() {
             int sum = 0;
-            for (int i = 0; i < Get_count_of_flats_on_floor(); i++)
-            {
+            for (int i = 0; i < Get_count_of_flats_on_floor(); i++) {
                 sum += flats_on_floor.get(i).square;
             }
             return sum;
+        }
+
+        public Flat atFlat(int index) {
+            return flats_on_floor.get(index);
+        }
+
+        public void printFloor(){
+            int count_of_flats_on_the_floor  = flats_on_floor.size();
+            for (Flat flat : flats_on_floor) {
+                flat.Print_Flat();
+                System.out.println("\n");
+            }
         }
     }
 
     House(int new_number_of_house) {
         count_of_floors = 0;
         count_of_flats = 0;
-        number_of_house = 0;
+        number_of_house = new_number_of_house;
     }
 
     public void Add_floor() {
         count_of_floors++;
         floors.add(new Floor(count_of_floors));
     }
-    public int Get_count_of_flats(){
+
+    public int Get_count_of_flats() {
         return count_of_flats;
     }
-    public int Get_count_of_floors(){
+
+    public int Get_count_of_floors() {
         return count_of_floors;
     }
-    public int Get_number_of_house(){
+
+    public int Get_number_of_house() {
         return number_of_house;
     }
-    public void Set_number_of_house(int new_number){
+
+    public void Set_number_of_house(int new_number) {
         number_of_house = new_number;
     }
-    public int Get_Count_Of_Humans(){
+
+    public int Get_Count_Of_Humans() {
         int sum = 0;
-        for (int i = 0; i < count_of_floors; i++){
+        for (int i = 0; i < count_of_floors; i++) {
             sum += floors.get(i).Get_Count_Of_Humans_On_Floor();
         }
         return sum;
     }
-    public int Get_Square(){
+
+    public int Get_Square() {
         int sum = 0;
-        for (int i = 0; i < count_of_floors; i++){
+        for (int i = 0; i < count_of_floors; i++) {
             sum += floors.get(i).Get_Square_Of_Floor();
         }
         return sum;
+    }
+
+    public Floor atFloor(int index) {
+        return floors.get(index);
+    }
+
+    public void printHouse() {
+        System.out.println("The House " + this.Get_number_of_house() + ":\n");
+        int count_of_floors = this.Get_count_of_floors();
+        for (int i = 0; i < count_of_floors; i++) {
+            System.out.println("     Floor " + (i + 1) + " :\n");
+            int count_of_flats_on_the_floor = this.floors.get(i).Get_count_of_flats_on_floor();
+            for (int j = 0; j < count_of_flats_on_the_floor; j++) {
+                this.atFloor(i).flats_on_floor.get(j).Print_Flat();
+                System.out.println("\n");
+            }
+        }
     }
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -7,99 +9,249 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        House house = new House(17);
-        System.out.println("The 'House 17' without floors and flats was built...");
-        short ask = 0;
-        short ask_1;
-        short ask_2;
+        ArrayList<House> Houses = new ArrayList<>();
+        int count_of_houses = 0;
 
-        while(ask == 0){
-            while (true) {
-                System.out.println(
-                                "1) Show whole House\n" +
-                                "2) Add Floor\n" +
-                                "3) Add Flat\n" +
-                                "4) Show full count of humans\n" +
-                                "5) Full square of House\n" +
-                                "6) Floor control panel\n" +
-                                "7) Flat control panel\n");
+        String command;
+        while (true) {
+            command = in.nextLine();
+            command = command.toLowerCase();
+            command = command.replaceAll(" ", "");
+            //System.out.println(command);
 
-                ask_1 = in.nextShort();
-                if (ask_1 <= 0 || ask_1 > 7){
-                    System.out.println("There is no such number. You should write 1-7");
+            if (command.contains("buildhouse")) {
+                Houses.add(new House(count_of_houses + 1));
+                System.out.println("The House " + (count_of_houses + 1) + " without floors and flats was build!");
+                count_of_houses++;
+                continue;
+            }
+
+            if (command.contains("showallhouses") || command.contains("showall")) {
+                if (count_of_houses == 0) {
+                    System.out.println("Count of houses == 0! You should build new house!");
+                    continue;
+                } else {
+                    for (int i = 0; i < count_of_houses; i++) {
+                        Houses.get(i).printHouse();
+                    }
+                }
+                continue;
+            }
+
+            if (command.contains("showhouse")){
+                command = command.replaceAll("[^0-9]", "");
+                int index = Integer.parseInt(command);
+                if (index > count_of_houses || index < 0){
+                    System.out.println("Wrong index!");
                     continue;
                 }
-                else break;
+                Houses.get(index -1).printHouse();
             }
-            switch (ask_1){
-                case 1:
-                    System.out.println("The House " + house.Get_number_of_house() + ":\n");
-                    int count_of_floors = house.Get_count_of_floors();
-                    for (int i = 0; i < count_of_floors; i++){
-                        System.out.println("     Floor " + (i+1) + " :\n");
-                        int count_of_flats_on_the_floor = house.floors.get(i).Get_count_of_flats_on_floor();
-                        for (int j = 0; j < count_of_flats_on_the_floor; j++){
-                            house.floors.get(i).flats_on_floor.get(j).Print_Flat();
-                            System.out.println("\n");
-                        }
-                    }
-                    break;
-                case 2:
-                    house.Add_floor();
-                    System.out.println("Floor added! The full count of floors: " + house.Get_count_of_floors());
-                    break;
-                case 3:
-                    int temp_floor;
-                    boolean flag = true;
 
-                    while (true) {
-                        if (house.Get_count_of_floors() == 0){
-                            short zero_floors;
-                            System.out.println("There are 0 floors! Can we create a new floor and add flat after them?(0) - yes; 1) - no)");
-                            zero_floors = in.nextShort();
-                            if (zero_floors == 0){
-                              house.Add_floor();
-                              System.out.println("Floor added! The full count of floors: " + house.Get_count_of_floors());
-                            }
-                            else{
-                                
-                            }
-                        }
-                        System.out.println("In what floor you want to place a new flat? (Count of floors: " + house.Get_count_of_floors() + ")\n");
-                        temp_floor = in.nextInt();
-                        if (temp_floor > house.Get_count_of_floors() || temp_floor <= 0) {
-                            System.out.println("Wrong number of floor! Try again!");
-                            continue;
-                        }
-                        else break;
-                    }
-
-                    int new_count_of_humans;
-                    int new_count_of_rooms;
-                    double new_square;
-                    int new_price;
-
-                    System.out.println("Count of humans: ");
-                    new_count_of_humans = in.nextInt();
-                    System.out.println("Count of rooms: ");
-                    new_count_of_rooms = in.nextInt();
-                    System.out.println("Square: ");
-                    new_square = in.nextDouble();
-                    System.out.println("Price: ");
-                    new_price = in.nextInt();
-                    house.floors.get(temp_floor-1).Add_flat(new_count_of_humans,  new_count_of_rooms, new_square, new_price);
-
-                    System.out.println("New flat added. Current count of flats:" + house.Get_count_of_flats());
-                    break;
-                case 4:
-                    System.out.println("Count of Humans in House: " + house.Get_Count_Of_Humans());
-                    break;
-                case 5:
-                    System.out.println("Whole square of House: " + house.Get_Square());
+            if (command.contains("addfloor")){
+                command = command.replaceAll("[^0-9]", "");
+                int index = Integer.parseInt(command);
+                System.out.println("index: " + index);
+                if (index > count_of_houses || index < 0){
+                    System.out.println("Wrong index!");
+                    continue;
+                }
+                Houses.get(index-1).Add_floor();
+                System.out.println("Floor on house " + (index) + " was added!");
             }
-            System.out.println("Continue? 0) - yes; 1) - no\n");
-            ask = in.nextShort();
+
+            if (command.contains("controlpanel")){
+                command = command.replaceAll("[^0-9]", "");
+                int index = Integer.parseInt(command);
+                if (index > count_of_houses || index < 0){
+                    System.out.println("Wrong index!");
+                    continue;
+                }
+                while (true){
+                    short ask;
+                    System.out.println("1) Show whole House\n" +
+                            "2) Add Floor\n" +
+                            "3) Add Flat\n" +
+                            "4) Show full count of humans\n" +
+                            "5) Full square of House\n" +
+                            "6) Floor control panel\n" +
+                            "7) Exit\n");
+
+                    ask = in.nextShort();
+                    if (ask <= 0 || ask > 7){
+                        System.out.println("Wrong number! Try again!");
+                        continue;
+                    }
+                    if (ask == 7){
+                        break;
+                    }
+                    switch (ask) {
+                        case 1:
+                            Houses.get(index - 1).printHouse();
+                            break;
+                        case 2:
+                            Houses.get(index - 1).Add_floor();
+                            System.out.println("Floor on house " + (index) + " was added!");
+                            break;
+                        case 3:
+                            while (true) {
+                                System.out.println("In what floor you want to add new flat? (1 - " + Houses.get(index - 1).Get_count_of_floors() + ")");
+                                int temp_floor;
+                                temp_floor = in.nextInt();
+                                if (temp_floor <= 0 || temp_floor > Houses.get(index - 1).Get_count_of_floors()) {
+                                    System.out.println("Wrong number of floor! Count of floors in this house are " + Houses.get(index - 1).Get_count_of_floors() + ". Try again!");
+                                } else {
+                                    int new_count_of_humans;
+                                    int new_count_of_rooms;
+                                    double new_square;
+                                    int new_price;
+
+                                    System.out.println("Count of humans: ");
+                                    new_count_of_humans = in.nextInt();
+                                    System.out.println("Count of rooms: ");
+                                    new_count_of_rooms = in.nextInt();
+                                    System.out.println("Square: ");
+                                    new_square = in.nextDouble();
+                                    System.out.println("Price: ");
+                                    new_price = in.nextInt();
+                                    Houses.get(index - 1).atFloor(temp_floor - 1).Add_flat(new_count_of_humans, new_count_of_rooms, new_square, new_price);
+
+                                    System.out.println("New flat added. Current count of flats:" + Houses.get(index - 1).Get_count_of_flats());
+                                    break;
+                                }
+                            }
+                            break;
+                        case 4:
+                            System.out.println("Count of Humans in House: " + Houses.get(index - 1).Get_Count_Of_Humans());
+                            break;
+                        case 5:
+                            System.out.println("Whole square of House: " + Houses.get(index - 1).Get_Square());
+                            break;
+                        case 6:
+                            boolean flag = true;
+                            while (flag) {
+                                System.out.println("In what floor you want to create? (1 - " + Houses.get(index - 1).Get_count_of_floors() + ")");
+                                int temp_floor;
+                                temp_floor = in.nextInt();
+                                if (temp_floor < 1 || temp_floor > Houses.get(index - 1).Get_count_of_floors()) {
+                                    System.out.println("Wrong number of floor! Try again!");
+                                    continue;
+                                }
+                                while (true) {
+                                    short ask_1;
+                                    System.out.println("1) Show whole Floor\n" +
+                                            "2) Add Flat\n" +
+                                            "3) Show full count of humans\n" +
+                                            "4) Full square of House\n" +
+                                            "5) Flat control panel\n" +
+                                            "6) Exit\n");
+                                    ask_1 = in.nextShort();
+                                    if (ask_1 < 1 || ask_1 > 6){
+                                        System.out.println("Wrong number! Try again!");
+                                        continue;
+                                    }
+                                    if (ask_1 == 6){
+                                        flag = false;
+                                        break;
+                                    }
+                                    switch (ask_1) {
+                                        case 1 -> {
+                                            System.out.println("    Flats:\n");
+                                            Houses.get(index - 1).atFloor(temp_floor - 1).printFloor();
+                                        }
+                                        case 2 -> {
+                                            int new_count_of_humans;
+                                            int new_count_of_rooms;
+                                            double new_square;
+                                            int new_price;
+                                            System.out.println("Count of humans: ");
+                                            new_count_of_humans = in.nextInt();
+                                            System.out.println("Count of rooms: ");
+                                            new_count_of_rooms = in.nextInt();
+                                            System.out.println("Square: ");
+                                            new_square = in.nextDouble();
+                                            System.out.println("Price: ");
+                                            new_price = in.nextInt();
+                                            Houses.get(index - 1).atFloor(temp_floor - 1).Add_flat(new_count_of_humans, new_count_of_rooms, new_square, new_price);
+                                            System.out.println("New flat added. Current count of flats:" + Houses.get(index - 1).Get_count_of_flats());
+                                        }
+                                        case 3 -> {
+                                            System.out.println(Houses.get(index - 1).atFloor(temp_floor - 1).Get_Count_Of_Humans_On_Floor());
+                                        }
+                                        case 4 -> {
+                                            Houses.get(index - 1).atFloor(temp_floor - 1).Get_Square_Of_Floor();
+                                            System.out.println(Houses.get(index - 1).atFloor(temp_floor - 1).Get_Square_Of_Floor());
+                                        }
+                                        case 5 -> {
+                                            boolean flag_1 = true;
+                                            while (flag_1) {
+                                                short temp_flat;
+                                                System.out.println("Choose the flat on the floor (1 - " + Houses.get(index - 1).atFloor(temp_floor - 1).Get_count_of_flats_on_floor());
+                                                temp_flat = in.nextShort();
+                                                if (temp_flat < 1 || temp_flat > Houses.get(index - 1).atFloor(temp_floor - 1).Get_count_of_flats_on_floor()) {
+                                                    System.out.println("Wrong number of flat! Try again!");
+                                                    continue;
+                                                }
+                                                while (true) {
+                                                    short ask_2;
+                                                    System.out.println("1) Set count of humans\n2) Set count of rooms\n3) Set square\n4) Set price\n5) Exit\n");
+                                                    ask_2 = in.nextShort();
+                                                    if (ask_2 < 1 || ask_2 > 5) {
+                                                        System.out.println("Wrong number! Try again!");
+                                                        continue;
+                                                    }
+                                                    if (ask_2 == 5) {
+                                                        flag_1 = false;
+                                                        break;
+                                                    }
+                                                    switch (ask_2) {
+                                                        case 1 -> {
+                                                            int new_count_of_humans_2;
+                                                            System.out.println("Count of humans: ");
+                                                            new_count_of_humans_2 = in.nextInt();
+                                                            Houses.get(index - 1).atFloor(temp_floor - 1).atFlat(temp_flat - 1).Set_count_of_humans(new_count_of_humans_2);
+                                                        }
+                                                        case 2 -> {
+                                                            int new_count_of_rooms_2;
+                                                            System.out.println("Count of rooms: ");
+                                                            new_count_of_rooms_2 = in.nextInt();
+                                                            Houses.get(index - 1).atFloor(temp_floor - 1).atFlat(temp_flat - 1).Set_count_of_rooms(new_count_of_rooms_2);
+                                                        }
+                                                        case 3 -> {
+                                                            double new_square_2;
+                                                            System.out.println("Square: ");
+                                                            new_square_2 = in.nextDouble();
+                                                            Houses.get(index - 1).atFloor(temp_floor - 1).atFlat(temp_flat - 1).Set_square(new_square_2);
+                                                        }
+                                                        case 4 -> {
+                                                            int new_price_2;
+                                                            System.out.println("Price: ");
+                                                            new_price_2 = in.nextInt();
+                                                            Houses.get(index - 1).atFloor(temp_floor - 1).atFlat(temp_flat - 1).Set_price(new_price_2);
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                    }
+                }
+            }
+
+            if (command.contains("exit")){
+                break;
+            }
         }
     }
 }
+
+
+
+
+
+
+
+ 
 
